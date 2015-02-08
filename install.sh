16a7e520eda09f479c29ec50690a6251569e0b53
Niles Rogoff <nilesrogoff@gmail.com>
2015-02-07 19:33:30 -0500

    Moved to fish

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ git submodule init && git submodule update
 
 # Remove all dotfiles from the home directory if present.
 echo Removing any existing dotfiles from your home directory.
-rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme ~/.tmux
+rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme ~/.tmux ~/.config/fish/config.fish
 
 # Initialize symlinks.
 echo Creating symlinks in your home directory that point to this dotfiles repository.
 ln -s "$PWD/.vim" ~/.vim
 ln -s "$PWD/.vim/vimrc" ~/.vimrc
+ln -s "$PWD/.config.fish" ~/.config/fish/config.fish
 ln -s "$PWD/.bashrc" ~/.bashrc
 ln -s "$PWD/.bash_profile" ~/.bash_profile
 ln -s "$PWD/.inputrc" ~/.inputrc
