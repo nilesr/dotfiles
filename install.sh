79527ae1991a13dd97391b06b9615db66270bbb7
Niles Rogoff <me@niles.xyz>
2017-02-06 22:00:47 -0500

    Fix installer for real now

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shel
 echo Creating symlinks in your home directory that point to this dotfiles repository.
 ln -s "$PWD/.vim" ~/.vim
 ln -s "$PWD/.vim/vimrc" ~/.vimrc
-ln -s "$PWD/.vizuki.vim" ~/.vim/colors/vizuki.vim
+ln -s "$PWD/.vizuki.vim" ~/.vim/colors/vizuki.vim &> /dev/null
 mkdir -p ~/.config/fish
 ln -s "$PWD/.config.fish" ~/.config/fish/config.fish
 ln -s "$PWD/.bashrc" ~/.bashrc
@@ -26,8 +26,8 @@ ln -s "$PWD/.tmux.conf" ~/.tmux.conf
 ln -s "$PWD/.tmux_theme" ~/.tmux_theme
 ln -s "$PWD/.tmux" ~/.tmux
 ln -s "$PWD/.putty.sh" ~/.putty.sh
-#echo Enabling apt progress bar
-#echo 'Dpkg::Progress-Fancy "1";' | sudo tee /etc/apt/apt.conf.d/99progressbar
+echo Enabling apt progress bar
+echo 'Dpkg::Progress-Fancy "1";' | sudo tee /etc/apt/apt.conf.d/99progressbar
 echo Changing lightline vim theme
 sed -i 's/107/108/' "$PWD/.vim/bundle/lightline.vim/autoload/lightline/colorscheme/jellybeans.vim"
 
@@ -40,18 +40,16 @@ gsettings set org.gnome.nautilus.preferences enable-interactive-search true
 echo Backing up /etc/issue
 sudo mv /etc/issue /etc/issue.bac
 echo Installing banner
-sudo tee /etc/issue < "$PWD"/.issue >/dev/null
+sudo tee /etc/issue < "$PWD"/.issue >/dev/null # lint dings me for SC2024 (sudo doesn't affect redirects) here, but I don't need sudo to read from ~/.dotfiles/.issue, so I don't know what it thinks it's warning me about
 grep -v Banner /etc/ssh/sshd_config|sudo tee /etc/ssh/sshd_config_new > /dev/null
 sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bac
 sudo mv /etc/ssh/sshd_config_new /etc/ssh/sshd_config
 echo "Banner /etc/issue" | sudo tee -a /etc/ssh/sshd_config
 echo Forwarding journald to tty12
-sudo mkdir /etc/systemd/journald.conf.d
-cat| sudo tee /etc/systemd/journald.conf.d/fw-tty12.conf << EOF
-[Journal]
-ForwardToConsole=yes
-TTYPath=/dev/tty12
-MaxLevelConsole=info
-EOF
+sudo mkdir /etc/systemd/journald.conf.d &> /dev/null # don't complain if it already exists (it should)
+sudo tee /etc/systemd/journald.conf.d/fw-tty12.conf <<< '[Journal]'
+sudo tee -a /etc/systemd/journald.conf.d/fw-tty12.conf <<< 'ForwardToConsole=yes'
+sudo tee -a /etc/systemd/journald.conf.d/fw-tty12.conf <<< 'TTYPath=/dev/tty12'
+sudo tee -a /etc/systemd/journald.conf.d/fw-tty12.conf <<< 'MaxLevelConsole=info'
 # Finished.
 echo Dotfiles installation complete.
