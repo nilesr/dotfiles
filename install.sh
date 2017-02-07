45c005c5595c18821bf8ac9b08e2c04cbb1b002c
Niles Rogoff <me@niles.xyz>
2017-02-06 23:55:31 -0500

    Testing right now

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ ln -s "$PWD/.inputrc" ~/.inputrc
 ln -s "$PWD/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.shell_prompt.sh" ~/.shell_prompt.sh
 ln -s "$PWD/.tmux.conf" ~/.tmux.conf
+ln -s "$PWD/.tmux_plugins.conf" ~/.tmux_plugins.conf
 ln -s "$PWD/.tmux_theme" ~/.tmux_theme
 ln -s "$PWD/.tmux_theme.insert" ~/.tmux_theme.insert
 ln -s "$PWD/.tmux_theme.normal" ~/.tmux_theme.normal
