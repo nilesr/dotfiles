3791e1c6295c78eb740796ed2159959fd46832fe
Niles Rogoff <me@niles.xyz>
2017-06-24 18:12:31 -0700

    Better mac support

--- .config.fish
+++ .config.fish
@@ -1,7 +1,11 @@
 # Machine specific instructions, not on dotfiles repo
 test -f "$HOME"/.fishrc; and source "$HOME"/.fishrc
-# Prompt
-alias grep=/usr/bin/env\ grep\ -P\ --color=always
+if grep --version|grep -q 'GNU grep' ^/dev/null
+	alias grep="grep -P --color=always"
+else
+	# BSD grep doesn't like -P
+	alias grep="grep --color=always"
+end
 
 # Solarized Dark 256 dircolors
 setenv LS_COLORS 'no=00;38;5;244:rs=0:di=00;38;5;33:ln=00;38;5;37:mh=00:pi=48;5;230;38;5;136;01:so=48;5;230;38;5;136;01:do=48;5;230;38;5;136;01:bd=48;5;230;38;5;244;01:cd=48;5;230;38;5;244;01:or=48;5;235;38;5;160:su=48;5;160;38;5;230:sg=48;5;136;38;5;230:ca=30;41:tw=48;5;64;38;5;230:ow=48;5;235;38;5;33:st=48;5;33;38;5;230:ex=00;38;5;64:*.tar=00;38;5;61:*.tgz=00;38;5;61:*.arj=00;38;5;61:*.taz=00;38;5;61:*.lzh=00;38;5;61:*.lzma=00;38;5;61:*.tlz=00;38;5;61:*.txz=00;38;5;61:*.zip=00;38;5;61:*.z=00;38;5;61:*.Z=00;38;5;61:*.dz=00;38;5;61:*.gz=00;38;5;61:*.lz=00;38;5;61:*.xz=00;38;5;61:*.bz2=00;38;5;61:*.bz=00;38;5;61:*.tbz=00;38;5;61:*.tbz2=00;38;5;61:*.tz=00;38;5;61:*.deb=00;38;5;61:*.rpm=00;38;5;61:*.jar=00;38;5;61:*.rar=00;38;5;61:*.ace=00;38;5;61:*.zoo=00;38;5;61:*.cpio=00;38;5;61:*.7z=00;38;5;61:*.rz=00;38;5;61:*.apk=00;38;5;61:*.gem=00;38;5;61:*.jpg=00;38;5;136:*.JPG=00;38;5;136:*.jpeg=00;38;5;136:*.gif=00;38;5;136:*.bmp=00;38;5;136:*.pbm=00;38;5;136:*.pgm=00;38;5;136:*.ppm=00;38;5;136:*.tga=00;38;5;136:*.xbm=00;38;5;136:*.xpm=00;38;5;136:*.tif=00;38;5;136:*.tiff=00;38;5;136:*.png=00;38;5;136:*.PNG=00;38;5;136:*.svg=00;38;5;136:*.svgz=00;38;5;136:*.mng=00;38;5;136:*.pcx=00;38;5;136:*.dl=00;38;5;136:*.xcf=00;38;5;136:*.xwd=00;38;5;136:*.yuv=00;38;5;136:*.cgm=00;38;5;136:*.emf=00;38;5;136:*.eps=00;38;5;136:*.CR2=00;38;5;136:*.ico=00;38;5;136:*.tex=00;38;5;245:*.rdf=00;38;5;245:*.owl=00;38;5;245:*.n3=00;38;5;245:*.ttl=00;38;5;245:*.nt=00;38;5;245:*.torrent=00;38;5;245:*.xml=00;38;5;245:*Makefile=00;38;5;245:*Rakefile=00;38;5;245:*Dockerfile=00;38;5;245:*build.xml=00;38;5;245:*rc=00;38;5;245:*1=00;38;5;245:*.nfo=00;38;5;245:*README=00;38;5;245:*README.txt=00;38;5;245:*readme.txt=00;38;5;245:*.md=00;38;5;245:*README.markdown=00;38;5;245:*.ini=00;38;5;245:*.yml=00;38;5;245:*.cfg=00;38;5;245:*.conf=00;38;5;245:*.c=00;38;5;245:*.cpp=00;38;5;245:*.cc=00;38;5;245:*.sqlite=00;38;5;245:*.go=00;38;5;245:*.log=00;38;5;240:*.bak=00;38;5;240:*.aux=00;38;5;240:*.lof=00;38;5;240:*.lol=00;38;5;240:*.lot=00;38;5;240:*.out=00;38;5;240:*.toc=00;38;5;240:*.bbl=00;38;5;240:*.blg=00;38;5;240:*~=00;38;5;240:*#=00;38;5;240:*.part=00;38;5;240:*.incomplete=00;38;5;240:*.swp=00;38;5;240:*.tmp=00;38;5;240:*.temp=00;38;5;240:*.o=00;38;5;240:*.pyc=00;38;5;240:*.class=00;38;5;240:*.cache=00;38;5;240:*.aac=00;38;5;166:*.au=00;38;5;166:*.flac=00;38;5;166:*.mid=00;38;5;166:*.midi=00;38;5;166:*.mka=00;38;5;166:*.mp3=00;38;5;166:*.mpc=00;38;5;166:*.ogg=00;38;5;166:*.ra=00;38;5;166:*.wav=00;38;5;166:*.m4a=00;38;5;166:*.axa=00;38;5;166:*.oga=00;38;5;166:*.spx=00;38;5;166:*.xspf=00;38;5;166:*.mov=00;38;5;166:*.MOV=00;38;5;166:*.mpg=00;38;5;166:*.mpeg=00;38;5;166:*.m2v=00;38;5;166:*.mkv=00;38;5;166:*.ogm=00;38;5;166:*.mp4=00;38;5;166:*.m4v=00;38;5;166:*.mp4v=00;38;5;166:*.vob=00;38;5;166:*.qt=00;38;5;166:*.nuv=00;38;5;166:*.wmv=00;38;5;166:*.asf=00;38;5;166:*.rm=00;38;5;166:*.rmvb=00;38;5;166:*.flc=00;38;5;166:*.avi=00;38;5;166:*.fli=00;38;5;166:*.flv=00;38;5;166:*.gl=00;38;5;166:*.m2ts=00;38;5;166:*.divx=00;38;5;166:*.webm=00;38;5;166:*.axv=00;38;5;166:*.anx=00;38;5;166:*.ogv=00;38;5;166:*.ogx=00;38;5;166:'
@@ -79,10 +83,17 @@ end
 # make these global
 #set line 0
 set online true
+function has_default_route
+	if uname|grep -q Darwin # if we're on mac
+		route get 8.8.8.8 | grep -q gateway # untested
+	else # if we're in a sane environment
+		route -n|grep -q '^0.0.0.0'
+	end
+end
 function login_message
 	# ghetto as fuck, just check if there's a defeault route
 	#if route|grep -iq default;
-	if route -n|grep -q '^0.0.0.0'
+	if has_default_route
 		set online true
 	else
 		set online false
@@ -90,7 +101,9 @@ function login_message
 	# Print cpu usage
 	#printf '%s ' (cat /tmp/cpu)
 	# print ram usage
-	set statline (math (free|grep Mem|awk '{print "100*"$3"/"$2}'))
+	if which free >/dev/null ^/dev/null # mac doesn't have the free command
+		set statline (math (free|grep Mem|awk '{print "100*"$3"/"$2}'))
+	end
 	# Print disk usage
 	set statline "$statline "( df -h / /home|tail -n 2|awk '{print $5}'|tr -d '%'|tr '\n' ' ')
 	# Display users on login
@@ -283,7 +296,7 @@ function viopen
 	set online false
 	while test "$online" = "false"; 
 		sleep 1
-		sudo route -n|grep -q '^0.0.0.0'; and set online true
+		has_default_route; and set online true
 	end
 	echo "default route acquired, attempting to resolve hosts"
 	# if we can resolve names and ping out, we're done
@@ -294,6 +307,7 @@ function viopen
 	echo "Swap dnscrypt for dnsmasq? Enter to continue"
 	read nothing
 	sudo systemctl stop dnscrypt; sudo systemctl start dnsmasq
+	# this doesn't work on mac beacuse it uses BSD route instead of the normal one, but there's no NetworkManager on mac so who cares
 	echo nameserver (route -n|grep '^0.0.0.0'|awk '{print $2}') | sudo tee /etc/resolv.conf
 	echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf > /dev/null # -a for append
 	echo nameserver 127.0.0.1 | sudo tee -a /etc/resolv.conf > /dev/null # use freenic via dnsmasq if all else fails
